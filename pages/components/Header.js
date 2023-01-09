2f62b5b212088b98abbd5ac21bd9f27c340246a9
einstein07 <mkhsin035@myuct.ac.za>
2023-01-09 17:09:58 +0200

    Complete Header component description and accompanying styling

---
+++ pages/components/Header.js
@@ -0,0 +1,20 @@
+import styles from "../styles/Header.module.css";
+
+export default function Header() {
+  return (
+    <main className={styles.header}>
+      <img src="/assets/images/sindiso.jpg" className={styles.dp} />
+
+      <h1 className={styles.name}>Sindiso Mkhatshwa</h1>
+
+      <p className={styles.about}>
+        Electrical & Computer Engineer (Hons),
+        <br />
+        MSc Candidate in Computer Science at{" "}
+        <a href="http://www.nitschke-lab.uct.ac.za/" target="_blank" rel="noreferrer">
+          UCT
+        </a>
+      </p>
+    </main>
+  );
+}
